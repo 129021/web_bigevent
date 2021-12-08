bf7b38c57dfab5f4861077a05357d59a2442f55b
129021 <wangjianqin1@yahoo.com>
2021-12-08 17:03:45 +0800

    finished the module of login and register

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,6 +16,8 @@ $(function () {
     // 从layui中获取form对象
     var form = layui.form
 
+    var layer=layui.layer
+
     // 通过form.verify()函数来自定义校验规则
     form.verify({
 
@@ -43,14 +45,48 @@ $(function () {
     // 监听注册表单的提交事件
     $('#form_reg').on('submit', function (e) {
         e.preventDefault()
-        $.post('http://api-breakingnews-web.itheima.net/api/reguser', {
+        var data={
             username: $('#form_reg [name=username]').val(),
             password: $('#form_reg [name=password]').val()
-        }, function (res) {
+        }
+        $.post('/api/reguser',data , function (res) {
             if (res.stauts !== 0) {
-                return console.log(res.message);
+                return layer.msg(res.message);
             }
-            console.log('注册成功');
+            layer.msg('注册成功，请登录')
+
+            // 模拟人的点击行为
+            // $('#link_login').click()
+            $('#link_login').click()
+        })
+    })
+
+
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function(e){
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url:'/api/login',
+            method: 'POST',
+
+            // 快速获取表单中的数据
+            data:$(this).serialize(),
+            success:function(res){
+                if (res.status!==0){
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功!')
+                // console.log(res.token);
+
+
+                // 将登录成功的token字符串，保存到localStorage
+                localStorage.setItem('token',res.token)
+
+                // 跳转到后台主页
+                location.href='/index.html'
+            }
         })
     })
 })
