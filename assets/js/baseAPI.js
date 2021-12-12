85ab52dc604e28860a194d2a6b8ba2d632e1bd9b
129021 <wangjianqin1@yahoo.com>
2021-12-12 15:41:20 +0800

    Replace the API interface with my own

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,7 +5,10 @@ $.ajaxPrefilter(function (options) {
     // console.log(options.url);
 
     // 在发起真正的ajax请求之前统一拼接请求的根路径
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    // options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+
+    options.url = 'http://127.0.0.1:3007' + options.url
+
 
 
     // 统一为有权限的接口，设置headers请求头
